dd68acee4c92763c6c384f43c28c2c82ec67e5da
Shalini <shalini.singh@smartsheet.com>
2020-09-23 02:08:03 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# db-activity-tracker
---
+++ src/test/java/com/yellow/mango/dbactivitytracker/DbActivityTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yellow.mango.dbactivitytracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbActivityTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
